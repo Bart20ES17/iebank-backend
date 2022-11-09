9e2f5854570093c07b23a2d2943de6d5fb649c35
Barto17-PL <bartekrzycki@gmail.com>
2022-11-09 20:32:13 +0100

    first commit

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -1,32 +1,47 @@
+# Write tests for al routes
 from iebank_api import app
 import pytest
 
+
 def test_get_accounts(testing_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/accounts' page is requested (GET)
-    THEN check the response is valid
-    """
+    
     response = testing_client.get('/accounts')
     assert response.status_code == 200
 
+
 def test_dummy_wrong_path():
-    """
-    GIVEN a Flask application
-    WHEN the '/wrong_path' page is requested (GET)
-    THEN check the response is valid
-    """
+    
     with app.test_client() as client:
         response = client.get('/wrong_path')
         assert response.status_code == 404
 
+
 def test_create_account(testing_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/accounts' page is posted to (POST)
-    THEN check the response is valid
-    """
-    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
+    
+    response = testing_client.post(
+        '/accounts', json={'name': 'Joe Doe', 'currency': '€'})
+    assert response.status_code == 200
+    assert response.json['name'] == 'Joe Doe'
+    assert response.json['currency'] == '€'
+
+
+def test_delete_account(testing_client):
+
+    response = testing_client.delete('/accounts/1')
     assert response.status_code == 200
 
 
+def test_update_account(testing_client):
+
+    update = testing_client.put(
+        '/accounts/1', json={'name': 'Joe Doe', 'currency': '€'})
+    assert update.status_code == 200
+    assert update.json['name'] == 'Joe Doe'
+    assert update.json['currency'] == '€'
+
+
+def get_account(testing_client):
+    response = testing_client.get('/accounts/1')
+    assert response.status_code == 200
+    assert response.json['name'] == 'Joe Doe'
+    assert response.json['currency'] == '€'
